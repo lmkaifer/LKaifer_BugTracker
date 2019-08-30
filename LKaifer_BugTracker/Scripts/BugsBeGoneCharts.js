d9f945f8b6cffd0138570c91f1b78f990b28d5f6
lmkaifer <52322859+lmkaifer@users.noreply.github.com>
2019-08-30 09:27:05 -0400

    Chart Updates
    Charts updated on main dashboard

---
+++ LKaifer_BugTracker/Scripts/BugsBeGoneCharts.js
@@ -0,0 +1,41 @@
+﻿//3d Colorful Bar Chart - Task Load Chart
+function devtaskloadchart(data) {
+    var chart = AmCharts.makeChart("chartdiv", {
+        "theme": "light",
+        "type": "serial",
+        "startDuration": 2,
+        "dataProvider": data,
+        "valueAxes": [{
+            "position": "left",
+            "axisAlpha": 0,
+            "gridAlpha": 0
+        }],
+        "graphs": [{
+            "balloonText": "[[category]]: <b>[[value]]</b>",
+            "colorField": "color",
+            "fillAlphas": 0.85,
+            "lineAlpha": 0.1,
+            "type": "column",
+            "topRadius": 1,
+            "valueField": "visits"
+        }],
+        "depth3D": 40,
+        "angle": 30,
+        "chartCursor": {
+            "categoryBalloonEnabled": false,
+            "cursorAlpha": 0,
+            "zoomable": false
+        },
+        "categoryField": "country",
+        "categoryAxis": {
+            "gridPosition": "start",
+            "axisAlpha": 0,
+            "gridAlpha": 0
+
+        },
+        "export": {
+            "enabled": true
+        }
+
+    }, 0);
+}
